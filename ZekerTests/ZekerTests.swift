694029b97c3530e126b75ca0ede56803283adb7f
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2020-02-02 13:32:57 +0100

    Initial Commit

---
+++ ZekerTests/ZekerTests.swift
@@ -0,0 +1,34 @@
+//
+//  ZekerTests.swift
+//  ZekerTests
+//
+//  Created by Shaun Jacobsen on 02/02/2020.
+//  Copyright © 2020 Shaun Jacobsen. All rights reserved.
+//
+
+import XCTest
+@testable import Zeker
+
+class ZekerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

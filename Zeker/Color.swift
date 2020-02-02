3b452730ebceb80a389b4f1e0d4ad492a3f5dbd8
Shaun Jacobsen <shaun.d.jacobsen@gmail.com>
2020-02-02 20:31:11 +0100

    Creates custom colors

---
+++ Zeker/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  Zeker
+//
+//  Created by Shaun Jacobsen on 02/02/2020.
+//  Copyright © 2020 Shaun Jacobsen. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let backgroundColor = Color("Background Grey")
+    static let cardColor = Color("Card Grey")
+    static let shadowColor = Color("Shadow Grey")
+    static let firePink = Color("Fire Pink")
+    static let lightGrey = Color("Light Grey")
+    static let zekerGreen = Color("Zeker Green")
+    static let zekerGreenDark = Color("Zeker Green Dark")
+    static let amsterdamGrey = Color("Amsterdam Grey")
+}
